90ce0603db8721085a57dca5a6764d8033dd3c02
LZX <jack_lzx0221@163.com>
2022-07-12 00:06:37 +0900

    User name added

--- App.java
+++ App.java
@@ -1,6 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args){
+        System.out.println("Who are you?");
+        Scanner input=new Scanner(System.in);
+        String name=input.next();
+        System.out.println("Hello, "+name+"!");
+        input.close();
+
         System.out.println("Tossing a coin...");
         int H=0;
         int T=0;
