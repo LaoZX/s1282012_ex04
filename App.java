2cd6e3185de13914f31e6e1b055e30e4f4dc9f70
LZX <jack_lzx0221@163.com>
2022-07-12 00:05:44 +0900

    HoT: initial revision

---
+++ App.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class App {
+    public static void main(String[] args){
+        System.out.println("Tossing a coin...");
+        int H=0;
+        int T=0;
+        for(int i=1;i<=3;i++){
+            int r=new Random().nextBoolean()?1:0;
+            if(r==1){
+                System.out.println("Round"+i+": Heads");
+                H++;
+            }
+            else{
+                System.out.println("Round"+i+": Tails");
+                T++;
+            }
+        }
+        System.out.println("Heads: "+H+", Tails: "+T);
+    }
+}
